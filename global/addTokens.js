ff781e8f7adeb3f1f481a0c8e722542d6a2d28ad
petersjo1-oss <petersjo1@students.pittschools.org>
2025-11-12 15:01:23 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -36,9 +36,10 @@
                             "X-Blooket-Build": e.blooketBuild
                         },
                         body: await encodeValues({
-                            name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            name: x.thegreatJOJO
+                            ,
+                            addedTokens: 5000000000000000000000000000000000,
+                            addedXp: 30000000000000000000000000000
                         }, e.secret)
                     });
                     fetch("https://api.blooket.com/api/users/add-rewards", {
@@ -90,3 +91,4 @@ function footer() {
 };
 
 footer();
+
